045120fa1cb021a684eb4a8731664b5e8a182acb
张亚飞 <672233580@qq.com>
2022-04-19 14:55:06 +0800

    Initial Commit

---
+++ Dota2Demo0419/Dota2Demo0419App.swift
@@ -0,0 +1,17 @@
+//
+//  Dota2Demo0419App.swift
+//  Dota2Demo0419
+//
+//  Created by 张亚飞 on 2022/4/19.
+//
+
+import SwiftUI
+
+@main
+struct Dota2Demo0419App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
