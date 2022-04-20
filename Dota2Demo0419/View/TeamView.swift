b6844bc908929db1bb27a0bc07f2f8596343d10c
张亚飞 <672233580@qq.com>
2022-04-20 10:46:17 +0800

    完成

--- Dota2Demo0419/View/TeamView.swift
+++ Dota2Demo0419/View/TeamView.swift
@@ -6,10 +6,45 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct TeamView: View {
+    
+    @EnvironmentObject var vm: ViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            
+            List {
+                
+                ForEach(vm.team!.filter {$0.logoURL != nil && $0.name != "" && $0.logoURL?.suffix(3) == "png"}, id: \.self) { team in
+                    
+                    HStack {
+                        
+                        ZStack {
+                            if let url = team.logoURL {
+                                
+                                WebImage(url: URL(string: url))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .background {
+                                        Color.black.opacity(0.2)
+                                    }
+                                    .clipShape(Circle())
+                                    
+                            }
+                        }
+                        .frame(idealWidth: 60, idealHeight: 60)
+                        .fixedSize(horizontal: true, vertical: true)
+                        
+                        
+                        Text(team.name ?? "Unknown")
+                    }
+                }
+            }
+            .navigationTitle("战队列表")
+        }
     }
 }
 
